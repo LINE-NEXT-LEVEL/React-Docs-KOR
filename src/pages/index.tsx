008ec6a78915128909c0183e93ef5e4843a618b6
문혜현 <55529617+hyehyeonmoon@users.noreply.github.com>
2023-07-09 16:25:53 +0900

    Fix server build error (#52)
    * Remove unused file
    
    * Fix build error
    
    ---------
    
    Co-authored-by: hyehyeon-moon <hyehyeon.moon@linecorp.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
-import * as css from './index.css';
-
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
